ddeee3637582ce383d1dfab3c29998882810d95a
Dan Beck <danjacobbeck@gmail.com>
2022-02-22 17:52:09 -0800

    commit on origin/dan

--- client/src/pages/createAccount.js
+++ client/src/pages/createAccount.js
@@ -4,13 +4,13 @@ function createAccount(){
     return(
         <div>
             <div>
-                <h1>Create an Account</h1>
+                <h1 className="text-3xl font-bold">Create an Account</h1>
             </div>
             <div>
                 <form>
                     <input placeholder = "username" type = "username" id = "username" required/>
                     <input placeholder = "email@email.com" type = "email" id = "email" required/>
-                    <input placeholder = "password" type = "password" id = "password" required >
+                    <input placeholder = "password" type = "password" id = "password" required/>
                     <button type = "submit" > Done </button>
                     <Link to = './login'> If account already exists</Link>
                 </form>
