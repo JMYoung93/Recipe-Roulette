0ff94b321849c3c2af362605b59431c672c07128
Juwonoh41 <edmaster41@gmail.com>
2022-02-25 16:07:41 -0800

    adjusted code

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -21,12 +21,11 @@ export function Card({
         <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Recipe title</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            Where the recipe will go
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Save button</Button>
         </Card.Body>
       </Card>
         
