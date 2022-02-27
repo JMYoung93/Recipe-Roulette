54a9bcbf9a536a17f180ebb64295471be3fdde7b
Juwonoh41 <edmaster41@gmail.com>
2022-02-27 15:14:22 -0800

    Recipe card edited

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -5,12 +5,11 @@ function Recipe(){
         <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Recipe Title</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            Recipe
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Favorite</Button>
         </Card.Body>
       </Card>
     )
