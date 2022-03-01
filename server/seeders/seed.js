259d86ed22b61e4c858c39e9ecb10b537df2396d
Jeffrey <hernew@gmail.com>
2022-03-01 15:49:43 -0800

    added new logic to seed file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,15 @@
 let Profile = require('../models/Profile');
 let mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhostL27017', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Mongo Connection Open');
+})
+.catch((err) => {
+    console.log(err);
+})
 const profiles = [
     new Profile({
         name: "Caroline Pastorelli",
@@ -14,6 +23,7 @@ let done = 0;
 const seedData = async () => {
     try {
         await Profile.deleteMany ({});
+        await Profile.insertMany(profiles);
 
         for (let i = 0; i < profiles.length; i++) {
             profiles[i].save(function (err, result) {
