4c8c4dbc1c5ead3a90005bac75039822b760aa10
Dan Beck <danjacobbeck@gmail.com>
2022-03-03 22:52:34 -0800

    carried over carousel edits

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import {Card, Button, Carousel} from 'react-bootstrap'
 function Recipe(){
     return(
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+    <Carousel>
+      <Carousel.Item>
+        <Card style={{ width: '30rem' }}>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1583019107470-5bf8e4a96314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDcyfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60" />
         <Card.Body>
           <Card.Title>Recipe Title</Card.Title>
           <Card.Text>
@@ -12,6 +14,22 @@ function Recipe(){
           <Button variant="primary">Favorite</Button>
         </Card.Body>
       </Card>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <Card style={{ width: '30rem' }}>
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1643226228323-34e639efc40e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDk5fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60" />
+        <Card.Body>
+          <Card.Title>Recipe Title</Card.Title>
+          <Card.Text>
+            Recipe
+          </Card.Text>
+          <Button variant="primary">Favorite</Button>
+        </Card.Body>
+      </Card>
+      </Carousel.Item>
+    </Carousel>
     )
 }
+
 export default Recipe;
